03acd76a5f3e749b68a9c53477f535ef58b204a9
AlvaroA96 <alv.araya@duocuc.cl>
2025-03-22 18:54:55 -0300

    actulizado la validacion de cuenta

---
+++ Js/Register.js
@@ -0,0 +1,4 @@
+const name = document.getElementById('username')
+const mail = document.getElementById('mail')
+const password = document.getElementById('password')
+const password_confirmation = document.getElementById('confirm_password')
