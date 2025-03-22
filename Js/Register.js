025c0d7ad4944063e7498473154fa8398bb9cd02
AlvaroA96 <alv.araya@duocuc.cl>
2025-03-22 20:06:36 -0300

    v1.1

--- Js/Register.js
+++
@@ -1,4 +0,0 @@
-const name = document.getElementById('username')
-const mail = document.getElementById('mail')
-const password = document.getElementById('password')
-const password_confirmation = document.getElementById('confirm_password')
